3a8f887f9835ff83b78540d80e8dba472fa5a91c
Shan Hasan <shan119@hotmail.co.uk>
2015-01-01 17:19:07 +0000

    added menu tests

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,5 @@
 class Menu
 
+
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,16 +2,25 @@ require 'menu.rb'
 
 describe Menu do
   
-  it 'should let dishes be added to the list' do
+  let(:menu) {Menu.new}
+  let(:chicken) {double :dish}
+  let(:beef) (double :dish)
 
+  it 'should let dishes be added to the list' do
+    menu.add(chicken)
+    expect(menu.options).to eq [chicken]
   end
 
   it "should let dishes be deleted from the list" do
-
+    menu.add(chicken)
+    menu.delete(chicken)
+    expect(menu.options).to eq []
   end
 
   it "should show all current dishes" do
-
+    menu.add(chicken)
+    menu.add(beef)
+    menu.show
   end
 
 end

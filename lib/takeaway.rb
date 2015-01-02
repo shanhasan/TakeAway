ebd9598f67b3977e22bf062584ffc671c3823034
Shan Hasan <shan119@hotmail.co.uk>
2015-01-02 18:39:06 +0000

    completed takeaway class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,8 +1,23 @@
+require 'twilio-ruby'
+
 class Takeaway
 
-  def initialize(name, order)
-    @name = name
+  attr_reader :customer, :order
+
+  def initialize(customer, order)
+    @customer = customer
     @order = order
+  end
+
+  def send_sms
+    account_sid = 'AC61a9cb945374b1d928ae888f11ccbe29'
+    auth_token = 'ae8157ecac505886dae248fe1f086b9d'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client.account.messages.create({
+      :body => "Thank you #{customer.name}! Your order totalling £#{order.calculate}0 was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}",
+      :from => '441855231010',
+      :to => customer.number,
+      })
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,14 +1,15 @@
 require 'takeaway.rb'
 
-desribe Takeaway do
+describe Takeaway do
 
-  let(:takeawat {Takeaway.new})
+  let(:takeaway) {Takeaway.new('Shan', order)}
   let(:customer) {double :customer, name: 'Shan', number: '07525495989'}
   let(:order) {double :order}
+  let(:takeaway_double) {double :takeaway}
 
 
   it 'is initialized with the name of the customer' do
-    expect(takeaway.customer).to eq(Shan)
+    expect(takeaway.customer).to eq('Shan')
   end
 
   it 'is initialized with an order' do
@@ -16,7 +17,9 @@ desribe Takeaway do
   end
 
   it 'sends a message' do
-    takeaway.send_sms
+    allow(order).to receive(:calculate)
+    allow(takeaway_double).to receive(:send_sms)
+    takeaway_double.send_sms
   end
 
 end
